386e89e3292ea20cb49af90222858cf9693f0ed0
Andreja <andreja.kogovsek1@gmail.com>
2020-04-08 09:50:21 +0100

    Read in config

--- packages/www/next.config.js
+++ packages/www/next.config.js
@@ -1,6 +1,11 @@
 const withPlugins = require("next-compose-plugins");
 const withImages = require("next-images");
+const setupConfiguration = require("./config/app-configuration");
 
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    CONFIG: JSON.stringify(setupConfiguration()),
+  },
+};
 
 module.exports = withPlugins([[withImages]], nextConfig);
--- packages/www/src/pages/_app.tsx
+++ packages/www/src/pages/_app.tsx
@@ -1,6 +1,12 @@
 import { AppProps } from "next/app";
 import { Styled, ThemeProvider } from "theme-ui";
 import theme from "../styles";
+import * as firebase from "firebase/app";
+
+const config = JSON.parse(process.env.CONFIG);
+if (!firebase.apps.length) {
+  firebase.initializeApp(config.firebase);
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
