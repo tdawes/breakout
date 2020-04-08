1a2a308b5f243a4944e9421558b697a12f5208f7
Jake Runzer <jakerunzer@gmail.com>
2020-04-08 11:07:38 +0100

    fix layout of styles page

--- packages/www/src/pages/index.tsx
+++ packages/www/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { jsx, Styled, Box, Button, Input } from "theme-ui";
 import Layout from "../components/Layout";
 import Router from "next/router";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home = () => {
   const [roomId, setRoomId] = React.useState("");
@@ -19,31 +20,35 @@ const Home = () => {
           py: 0,
         }}
       >
-        <Header />
+        <Box sx={{ minHeight: "100vh" }}>
+          <Header />
 
-        <Box sx={{ pt: 4 }}>
-          <Styled.h1>Breakout Chat</Styled.h1>
+          <Box sx={{ pt: 4 }}>
+            <Styled.h1>Breakout Chat</Styled.h1>
 
-          <Box
-            as="form"
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (roomId.trim() !== "") {
-                Router.push("/room/[roomId]", `/room/${roomId}`);
-              }
-            }}
-          >
-            <Input
-              value={roomId}
-              placeholder="any text can be a room"
-              onChange={(e) => {
-                setRoomId(e.target.value);
+            <Box
+              as="form"
+              onSubmit={(e) => {
+                e.preventDefault();
+                if (roomId.trim() !== "") {
+                  Router.push("/room/[roomId]", `/room/${roomId}`);
+                }
               }}
-            />
+            >
+              <Input
+                value={roomId}
+                placeholder="any text can be a room"
+                onChange={(e) => {
+                  setRoomId(e.target.value);
+                }}
+              />
 
-            <Button sx={{ mt: 2 }}>Join</Button>
+              <Button sx={{ mt: 2 }}>Join</Button>
+            </Box>
           </Box>
         </Box>
+
+        <Footer />
       </Box>
     </Layout>
   );
