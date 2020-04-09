370db44f542a9aec0c74fc7474f6ba85d74b8b41
Jake Runzer <jakerunzer@gmail.com>
2020-04-09 15:58:52 +0100

    react-modal setup

---
+++ packages/www/src/components/Modal.tsx
@@ -0,0 +1,45 @@
+/** @jsx jsx */
+import * as React from "react";
+import { Box, Flex, jsx } from "theme-ui";
+import ReactModal from "react-modal";
+
+export interface Props {
+  isOpen: boolean;
+  onRequestClose: () => void;
+  allowClose?: boolean;
+}
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+ReactModal.setAppElement("#__next");
+
+const Modal: React.FC<Props> = (props) => {
+  return (
+    <ReactModal
+      isOpen={props.isOpen}
+      onRequestClose={props.onRequestClose}
+      style={customStyles}
+      shouldCloseOnEsc={props.allowClose}
+      shouldCloseOnOverlayClick={props.allowClose}
+    >
+      <Box
+        sx={{
+          color: "textDark",
+        }}
+      >
+        {props.children}
+      </Box>
+    </ReactModal>
+  );
+};
+
+export default Modal;
